c25853a9e215a6702227493ea3de718c886f3c07
Yichun Zhao <zhaoyichun@hotmail.com>
2024-09-08 02:37:05 +0200

    Removed the use of Future and directly used CompletableFuture.supplyAsync() for non-blocking task submission.

--- src/main/java/com.ynz.demo.asynccomputing/AsyncComputing.java
+++ src/main/java/com.ynz.demo.asynccomputing/AsyncComputing.java
@@ -1,16 +1,19 @@
 package com.ynz.demo.asynccomputing;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The AsyncComputing class demonstrates the use of CompletableFuture to run tasks asynchronously.
  */
+@Slf4j
 public class AsyncComputing {
-    private static final List<CompletableFuture<String>> futures = new ArrayList<>();
 
     /**
      * The main method is the entry point of the application.
@@ -18,38 +21,61 @@ public class AsyncComputing {
      * @param args Command line arguments
      */
     public static void main(String[] args) {
+        final List<CompletableFuture<String>> futures = new ArrayList<>();
+
         // Create a thread pool with a number of threads equal to the number of available processors
         var threadNumber = Runtime.getRuntime().availableProcessors();
         System.out.println("Number of available processors: " + threadNumber);
-
-        // Create a thread pool with a number of threads equal to the number of available processors
+        log.info("Number of available processors: {}", threadNumber);
         ExecutorService executorService = Executors.newFixedThreadPool(threadNumber);
 
         // Submit 18 tasks to the executor service, and run each task asynchronously and return completable future
         for (int i = 0; i < 18; i++) {
+            final int taskNumber = i;
 
-            // Run the task asynchronously and return a completable future
-            var future = CompletableFuture.supplyAsync(() -> {
+            // Create a completable future that will return the result of the future
+            var completableFuture = CompletableFuture.supplyAsync(() -> {
                 try {
-                    return new RandomNumberGeneratorTask().call();
+                    return new RandomNumberGeneratorTask(taskNumber).call();
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             }, executorService);
 
-            futures.add(future);
+            futures.add(completableFuture);
         }
+
+        // Wait for Task Completion Before Shutdown:
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        allFutures.join();
 
         // For each future, get the result and print it
         for (var future : futures) {
             future.thenAccept(System.out::println)
-                  .exceptionally(throwable -> {
-                      System.out.println(throwable.getMessage());
-                      return null;
-                  });
+                    .exceptionally(AsyncComputing::handleException);
         }
 
-        // Shutdown the executor service
+        // Shutdown the executor service gracefully
         executorService.shutdown();
+        try {
+            // Wait for the executor service to finish for 60 seconds
+            if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                // If the executor service is not finished after 60 seconds, shut it down forcefully
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+    }
+
+    /**
+     * Handles exceptions by printing the stack trace.
+     *
+     * @param throwable The throwable to handle
+     * @return null
+     */
+    private static Void handleException(Throwable throwable) {
+        log.error("Exception occurred: {}", throwable.getMessage());
+        return null;
     }
 }
--- src/main/java/com.ynz.demo.asynccomputing/RandomNumberGeneratorTask.java
+++ src/main/java/com.ynz.demo.asynccomputing/RandomNumberGeneratorTask.java
@@ -3,20 +3,42 @@ package com.ynz.demo.asynccomputing;
 import java.util.Random;
 import java.util.concurrent.Callable;
 
+/**
+ * The RandomNumberGeneratorTask class generates a random number after a delay.
+ */
 public class RandomNumberGeneratorTask implements Callable<String> {
+    private final int taskNumber;
 
+    /**
+     * Constructs a new RandomNumberGeneratorTask.
+     *
+     * @param taskNumber The task number
+     */
+    public RandomNumberGeneratorTask(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    /**
+     * Generates a random number after a delay.
+     *
+     * @return A string containing the task number, thread name, and generated random number
+     */
     @Override
     public String call() {
-
         try {
-            Thread.sleep(1000);
+            Thread.sleep(300);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        return Thread.currentThread().getName() + " : " + "Result: " + generateRandomNumber();
+        return String.format("taskNumber: %d %s : Result: %d", taskNumber, Thread.currentThread().getName(), generateRandomNumber());
     }
 
+    /**
+     * Generates a random number between 0 and 99.
+     *
+     * @return A random number between 0 and 99
+     */
     private int generateRandomNumber() {
         return new Random().nextInt(100);
     }
